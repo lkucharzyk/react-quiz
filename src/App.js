795076f11c15baf53f016ece5c8639013d519746
Łukasze <lukaszkucharzyk17@gmail.com>
2023-10-25 11:29:31 +0200

    addes question and answers displaying and handling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import { useEffect } from "react";
-import Header from "./Header";
-import Main from "./Main";
+import Header from "./components/Header";
+import Main from "./components/Main";
 import { useReducer } from "react";
-import Loader from "./Loader";
-import Error from "./Error";
-import StartScreen from "./StartScreen";
+import Loader from "./components/Loader";
+import Error from "./components/Error";
+import StartScreen from "./components/StartScreen";
+import Question from "./components/Question";
+import NextButton from "./components/NextButton";
 
 const initialState = {
   questions: [],
   status: "loading",
+  index: 0,
+  answer: null,
+  points: 0,
 };
 
 function reducer(state, action) {
@@ -17,6 +22,20 @@ function reducer(state, action) {
       return { ...state, questions: action.payload, status: "ready" };
     case "dataFailed":
       return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+    case "nextQuestion":
+      return { ...state, index: state.index + 1, answer: null };
 
     default:
       throw new Error("Action unkown");
@@ -24,7 +43,10 @@ function reducer(state, action) {
 }
 
 function App() {
-  const [{ status, questions }, dispatch] = useReducer(reducer, initialState);
+  const [{ status, questions, index, answer }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   const questionsQuanity = questions.length;
 
@@ -42,7 +64,20 @@ function App() {
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
         {status === "ready" && (
-          <StartScreen questionsQuanity={questionsQuanity} />
+          <StartScreen
+            questionsQuanity={questionsQuanity}
+            dispatch={() => dispatch({ type: "start" })}
+          />
+        )}
+        {status === "active" && (
+          <>
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextButton dispatch={dispatch} answer={answer} />
+          </>
         )}
       </Main>
     </div>
--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,9 +1,11 @@
-function StartScreen({ questionsQuanity }) {
+function StartScreen({ questionsQuanity, dispatch }) {
   return (
     <div className="start">
       <h2>Welcome to quiz!</h2>
       <h3>{questionsQuanity} questions to test your React knowledge</h3>
-      <button className="btn btn-ui">Let's start</button>
+      <button className="btn btn-ui" onClick={dispatch}>
+        Let's start
+      </button>
     </div>
   );
 }
